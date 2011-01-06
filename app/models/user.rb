a4934c2947948b1acc763bb4919da0846b97a17c
Sasa Ranisavljevic <sasa.ranisavljevic@gmail.com>
2011-01-06 15:48:53 +0100

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,66 @@
 # == Schema Information
-# Schema version: 20110104155831
+# Schema version: 20110106125544
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+ 
+  before_save :encrypt_password
 
-  validates :name, :presence => true, 
-                   :length => { :maximum => 50 },
-                   :uniqueness => { :case_sensitive => false}
-  validates :email, :presence => true, :format => { :with => email_regex }
+  validates :name, 
+    :presence   => true, 
+    :length     => { :maximum => 50 },
+    :uniqueness => { :case_sensitive => false}
+  
+  validates :email, 
+    :presence => true, 
+    :format => { :with => email_regex }
+
+  validates :password, 
+    :presence     => true, 
+    :confirmation => true,
+    :length       => { :within => 6..40 }
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.autenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+    # returns nil at the end of method which means that password is wrong
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
